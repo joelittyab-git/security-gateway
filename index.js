9d1b1fd17a7d71642e9f2dc8cbb331f971c5c26b
joelittyab-git <joeittab@gmail.com>
2024-08-21 16:57:09 +0530

    morgan logger

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const morgan = require("morgan");
+
 const authRouter = require("./routers/auth");
 const userRouter = require("./routers/user");
+
 const app = express();
 const PORT = 8080;
 
 // Middleware to parse JSON
 app.use(cookieParser());
 app.use(express.json());
+app.use(morgan("combined"));
+
 // Routers
 app.use("/auth",authRouter);
 app.use("/user", userRouter);
