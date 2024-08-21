851a7177c8062d6b09a61ec129b73ee409a5fcfb
joelittyab-git <joeittab@gmail.com>
2024-08-21 14:07:38 +0530

    docs

--- routers/user.js
+++ routers/user.js
@@ -2,6 +2,13 @@ const users = require("../services/users");
 const express = require("express");
 const router = express.Router()
 
+/**
+ * Basic registration endpoint. Registers the users and stores in the database
+ * POST "/user/register"
+ *   - Registers the username and password passed in the body as json data
+ *   - Responds with 
+ *        >200: If user registration is successful
+ */
 router.post("/register", (req,res)=>{
      const body = req.body;
      console.log(body);
